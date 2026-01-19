29a8550e2380df3e757437749ab3bc9ff4a95eb4
krsy0411-in-awslinux <krsy0411@example.com>
2026-01-19 05:03:57 +0000

    feat : create route table and association with subnets(public/private)

--- terraform/vpc-practice/vpc.tf
+++ terraform/vpc-practice/vpc.tf
@@ -35,3 +35,21 @@ resource "aws_internet_gateway" "igw" {
     Name = "terraform-syungg-igw"
   }
 }
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform-syungg-rt"
+  }
+}
+
+resource "aws_route_table_association" "route_table_public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "route_table_private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
