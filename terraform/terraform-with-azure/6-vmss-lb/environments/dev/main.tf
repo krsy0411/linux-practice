fd9b179d7121ac1d194a06f8f0f7987a8bc41f27
ThisTimeNull <krsy0411@naver.com>
2026-02-28 13:37:44 +0900

    feat(dev): VMSS 모듈을 로드 밸런서와 연결
    - VMSS 모듈 호출 추가
    - Web 서브넷에 VMSS 배치
    - VMSS를 Load Balancer 백엔드 풀에 자동 등록
    - 네트워크와 로드 밸런서 모듈 간 의존성 확립

--- terraform/terraform-with-azure/6-vmss-lb/environments/dev/main.tf
+++ terraform/terraform-with-azure/6-vmss-lb/environments/dev/main.tf
@@ -24,3 +24,31 @@ module "network" {
   web_subnet_cidr  = "10.10.1.0/24"
   db_subnet_cidr   = "10.10.2.0/24"
 }
+
+module "nsg" {
+  source = "../../modules/nsg"
+
+  resource_group_name = module.network.resource_group_name
+  location            = "Korea Central"
+
+  web_subnet_id  = module.network.web_subnet_id # newtork 모듈의 output을 nsg 모듈의 input으로 전달(모듈 간 의존성 연결)
+  db_subnet_id   = module.network.db_subnet_id # newtork 모듈의 output을 nsg 모듈의 input으로 전달(모듈 간 의존성 연결)
+  web_subnet_cidr = "10.10.1.0/24"
+}
+
+module "loadbalancer" {
+  source = "../../modules/loadbalancer"
+
+  resource_group_name = module.network.resource_group_name
+  location            = "Korea Central"
+  backend_subnet_id   = module.network.web_subnet_id
+}
+
+module "vmss" {
+  source = "../../modules/vmss"
+
+  resource_group_name = module.network.resource_group_name
+  location            = "Korea Central"
+  subnet_id           = module.network.web_subnet_id # VMSS → Web Subnet 안에 배치
+  backend_pool_id     = module.loadbalancer.backend_pool_id # VMSS → Load Balancer Backend Pool에 자동 등록
+}
